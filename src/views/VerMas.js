117e9a48e9d1fe102683795be8f350636dde4bc9
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-08-28 22:08:46 -0500

    actualizar: importando funciones para  filtros de ordenamiento

--- src/views/VerMas.js
+++ src/views/VerMas.js
@@ -1,9 +1,33 @@
-const seccVerMas = () => {
-    const verMas = document.createElement('section');
-    verMas.className = "verMas-contenido";
+import petsData from '../data/dataset.js';
+
+const seccVerMas = (props) => {
+    // pets.forEach((pet) => {
+    
+    const petName = props.nombre // Mittens
+
+    console.log('😊',petName);
+
+    const [pet]= petsData.filter(e=> e.name.toLowerCase()===petName.toLowerCase())
+    console.log("🚀 ~ //pets.forEach ~ pet:", pet)
+    // Mittens
+    //
+
+          // Convertir la edad a meses
+          const { years, months } = pet.facts.age;
+          const totalMonths = (years * 12) + months;
+      
+          let ageDisplay;
+          if (totalMonths < 12) {
+            ageDisplay = `${months} mes(es)`;
+          } else {
+            ageDisplay = `${years} año(s)`;
+          }
+          
+   const verMas = document.createElement('section');
+     verMas.className = "verMas-contenido";
     verMas.innerHTML = `
             <div class="verMas-container">
-            <i class="fa-solid fa-circle-xmark></i>
+            <i class="fa-solid fa-circle-xmark"></i>
                 <div class="verMas-izquierda">
                     <div itemprop="verMas-img">
                         <img class="verMas-img" src="${pet.imageUrl}" alt="${pet.name}">
@@ -12,7 +36,7 @@ const seccVerMas = () => {
                 </div>
                 <div class="verMas-derecha">
                     <div itemprop="description">${pet.description}</div>
-                    <div itemprop="age">Edad: ${ageDisplay}</div>
+                    <div itemprop="age">Edad: ${pet.ageDisplay}</div>
                     <div itemprop="gender">Género: ${pet.facts.gender}</div>
                     <div itemprop="breed">Raza: ${pet.facts.breed}</div>
                     <div itemprop="size">Tamaño: ${pet.facts.size}</div>
@@ -20,10 +44,12 @@ const seccVerMas = () => {
                     <div itemprop="curiosities">Curiosidades: ${pet.extraInfo.curiosities}</div>
                 </div>
         </section>
-                    `;
-    section.appendChild(petItem);
+    `;
+        // verMas.innerHTML='hola';
+        console.log(verMas);
+   // section.appendChild(petItem);
 
-return verMas;
+        return verMas;
 }
 
 export default seccVerMas;
