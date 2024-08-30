d3425420bbfe511ab473152a4998f419bce8567a
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-08-29 20:07:51 -0500

    actualizar: nueva vista VerMas.js implementando estructura y estilos para la nueva vista VerMas.

--- src/views/VerMas.js
+++ src/views/VerMas.js
@@ -3,36 +3,31 @@ import petsData from '../data/dataset.js';
 const seccVerMas = (props) => {
     // pets.forEach((pet) => {
     
-    const petName = props.nombre // Mittens
+  const petName = props.nombre // Mittens
 
-    console.log('😊',petName);
+  const [pet]= petsData.filter(e=> e.name.toLowerCase()===petName.toLowerCase())
 
-    const [pet]= petsData.filter(e=> e.name.toLowerCase()===petName.toLowerCase())
-    console.log("🚀 ~ //pets.forEach ~ pet:", pet)
-    // Mittens
-    //
+  // Convertir la edad a meses
+  const { years, months } = pet.facts.age;
+  const totalMonths = (years * 12) + months;
 
-          // Convertir la edad a meses
-          const { years, months } = pet.facts.age;
-          const totalMonths = (years * 12) + months;
-      
-          let ageDisplay;
-          if (totalMonths < 12) {
-            ageDisplay = `${months} mes(es)`;
-          } else {
-            ageDisplay = `${years} año(s)`;
-          }
+  let ageDisplay;
+  if (totalMonths < 12) {
+    ageDisplay = `${months} mes(es)`;
+  } else {
+    ageDisplay = `${years} año(s)`;
+  }
           
    const verMas = document.createElement('section');
-     verMas.className = "verMas-contenido";
+     verMas.className = "verMas";
     verMas.innerHTML = `
+          <i class="fa-solid fa-circle-xmark"></i>
             <div class="verMas-container">
-            <i class="fa-solid fa-circle-xmark"></i>
                 <div class="verMas-izquierda">
                     <div itemprop="verMas-img">
                         <img class="verMas-img" src="${pet.imageUrl}" alt="${pet.name}">
                     <div itemprop="name">${pet.name}</div>
-                    <button id="btn-chat">Chat<i class="fa-regular fa-comment fa-2xs" style="color: #ffffff;"></i></button>
+                    <a id="btn-chat" href="">Chat<i class="fa-regular fa-comment fa-2xs"></i></button>
                 </div>
                 <div class="verMas-derecha">
                     <div itemprop="description">${pet.description}</div>
@@ -43,6 +38,7 @@ const seccVerMas = (props) => {
                     <div itemprop="temperament">Comportamiento: ${pet.facts.temperament}</div>
                     <div itemprop="curiosities">Curiosidades: ${pet.extraInfo.curiosities}</div>
                 </div>
+            </div>
         </section>
     `;
         // verMas.innerHTML='hola';
