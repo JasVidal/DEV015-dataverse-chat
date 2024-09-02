ed775edb6bc3a5b7042683051ddd6d29572843b1
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-02 09:59:14 -0500

    implementar: agregando estructura y vistas para ApiKey

--- src/views/APIKey.js
+++ src/views/APIKey.js
@@ -1,14 +1,20 @@
-import Api from '../components/apikey.js';
-
+/* import Api from '../components/apikey.js';
+ */
 const pageApiKey = () => {
 
     const ApiKey = document.createElement('section');
   ApiKey.className = "pageApi";
-  ApiKey.id = "page-container";
+  ApiKey.innerHTML = `
 
-  pageApiKey.appendChild(Api());
+  <div>
+  <p>Hola</p><i type="password" placeholder="Agrega aquí tu contraseña></i>
+</div>
+`
+ /*  pageApiKey.appendChild(Api());
 
-
+  const apikey = document.createElement('section');
+    apikey.textContent = 'hola';
+   */
   return ApiKey;
 }
 
