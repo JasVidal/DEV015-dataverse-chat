0d08c74d37cf1390e139f5242139d1c82ad11794
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-02 21:01:07 -0500

    actualizar: actualizando estructura html de apikey

--- src/components/apikey.js
+++ src/components/apikey.js
@@ -9,7 +9,7 @@ const seccApiKey = () => {
   <div class="api-container">
   <img src="./assets/Logo.svg">
   <h1>Disfruta de  una conversación agradable con todas las mascotas de Monito o ten una linda charla y conoce más a tu mascota preferida. Ingresa tu API Key y descubre mucho más sobre ellos.</h1>
-  <input type="password" placeholder="Agrega aquí tu contraseña"></i>
+  <input type="password" placeholder="Ingresa aquí tu API Key"></i>
   <div class="botones"> 
     <button class="btn-limpiar">Limpiar</button>
     <button class="btn-guardar">Guardar</button>
