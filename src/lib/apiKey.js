882b0a3c004fe49b42ce7ca00dda3e71f0cfad6f
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-09 20:57:32 -0500

    actualizar: implementando localStorage con APIKey y test de prueba

--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -1,9 +1,13 @@
 export const getApiKey = () => {
-    alert( localStorage.getItem('test') ); // 1
-    // Implementa el código para obtener la API KEY desde Local Storage
- };
- 
- export const setApiKey = (key) => {
-    localStorage.setItem('test', 1);
-   // Implementa el código para guardar la API KEY en Local Storage
- };
+   // Implementa el código para obtener la API KEY desde Local Storage
+   const apiKey = localStorage.getItem('apiKey');
+   return apiKey
+ /*   return apiKey */
+};
+
+export const setApiKey = (key) => {
+  // Implementa el código para guardar la API KEY en Local Storage
+
+  const saveApiKey = localStorage.setItem('apiKey', key);
+  return saveApiKey
+};
--- test/apiKey.spec.js
+++ test/apiKey.spec.js
@@ -4,6 +4,8 @@ describe('getApiKey', () => {
 
   it('debería devolver el valor de la API Key', () => {
     // Desarrolla el test correspondiente aquí
+   const pruebaGetApi = getApiKey
+   expect(pruebaGetApi).toBe(getApiKey);
   });
 });
 
@@ -11,5 +13,7 @@ describe('setApiKey', () => {
 
   it('debería establecer correctamente la API Key', () => {
    // Desarrolla el test correspondiente aquí
+   const pruebaSetApi = setApiKey
+   expect(pruebaSetApi).toBe(setApiKey);
   });
 });
