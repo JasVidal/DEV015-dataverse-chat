9942e91c66ebffa3d01fc9d27ada418d22b84a01
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-11 17:58:27 -0500

    actualizar: integrando nueva función para borrar el apiKey

--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -1,13 +1,14 @@
 export const getApiKey = () => {
    // Implementa el código para obtener la API KEY desde Local Storage
-   const apiKey = localStorage.getItem('apiKey');
-   return apiKey
+   return localStorage.getItem('apiKey');
  /*   return apiKey */
 };
 
 export const setApiKey = (key) => {
   // Implementa el código para guardar la API KEY en Local Storage
+  localStorage.setItem('apiKey', key);
+};
 
-  const saveApiKey = localStorage.setItem('apiKey', key);
-  return saveApiKey
+export const deleteApiKey = () => {
+  return localStorage.removeItem('apiKey');
 };
