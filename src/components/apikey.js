1aa38aaa700348a6bd47022c4098e1e9913fb669
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-11 18:03:17 -0500

    actualizar: importando las funciones para guardar, recuperar y borrar API Key, a su vez implementando navigateTo e integrando los botones para navegar hacia las diferentes vistas en la pestaña API Key

--- src/components/apikey.js
+++ src/components/apikey.js
@@ -1,16 +1,17 @@
-import { getApiKey, setApiKey } from "../lib/apiKey.js";
+import { deleteApiKey, getApiKey, setApiKey } from "../lib/apiKey.js";
+import { navigateTo } from "../router.js";
 
 const seccApiKey = () => {
 
-  const ApiKey = document.createElement('section');
-  ApiKey.className = "apikey";
+  const contenedorApiKey = document.createElement('section');
+  contenedorApiKey.className = "apikey";
   
-  ApiKey.innerHTML = `
-  <i class="fa-solid fa-circle-xmark"></i>
+  contenedorApiKey.innerHTML = `
+  <i id="btn-cerrar" class="fa-solid fa-circle-xmark"></i>
   <div class="api-container">
   <img src="./assets/Logo.svg">
   <h1>Disfruta de  una conversación agradable con todas las mascotas de Monito o ten una linda charla y conoce más a tu mascota preferida. Ingresa tu API Key y descubre mucho más sobre ellos.</h1>
-  <input type="password" placeholder="Ingresa aquí tu API Key"></i>
+  <input class="input-api" type="password" placeholder="Ingresa aquí tu API Key"></i>
   <div class="botones"> 
     <button class="btn-limpiar">Limpiar</button>
     <button class="btn-guardar">Guardar</button>
@@ -18,10 +19,60 @@ const seccApiKey = () => {
     <button class="btn-regresar">Ir a Inicio</button>
   </div>`;
 
-  const obtenerApikey = getApiKey
-  const guardarApikey = setApiKey
 
-  return ApiKey;
+  const inputApikey = contenedorApiKey.querySelector('.input-api');
+  const btnGuardarApi = contenedorApiKey.querySelector('.btn-guardar');
+  const btnLimpiarApi = contenedorApiKey.querySelector('.btn-limpiar');
+ 
+
+  const staticApi = () => {
+    const obtenerApi = getApiKey();
+    if(obtenerApi) {
+      inputApikey.value = obtenerApi;
+    }
+  }
+  staticApi()
+
+  // Botón Guardar API Key //
+
+  btnGuardarApi.addEventListener('click', () => {
+  
+    // Guarda el valor en input //
+    setApiKey(inputApikey.value)
+
+    // Recupera lo guardado en setApiKey //
+    const recuperarApiKey = getApiKey();
+
+    if (recuperarApiKey) {
+      inputApikey.value = `${recuperarApiKey}`;
+    } else {
+      alert('Clave incorrecta')
+    }
+  }
+)  
+
+// Botón Limpiar API Key //
+
+btnLimpiarApi.addEventListener('click', () => {
+  deleteApiKey();
+  inputApikey.value = '';
+})
+
+// Botón volver a Home //
+
+const btnRegresar = contenedorApiKey.querySelector(".btn-regresar");
+        btnRegresar.addEventListener('click', () => {
+          navigateTo ("/")
+        } )     
+
+// Botón cerrar y volver al Home //
+
+  const btnCerrar = contenedorApiKey.querySelector("#btn-cerrar");
+        btnCerrar.addEventListener('click', () => {
+          navigateTo ("/")
+        } )     
+
+  return contenedorApiKey;
 }
 
 export default seccApiKey;
