d0a3ecfd0e771873a49a226fdaee273fa493d7ec
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-11 17:55:29 -0500

    implementando el rol y características para el elemento conocido como bot

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,9 +1,9 @@
 // Importa la función para obtener la API KEY desde apiKey.js
 import { getApiKey } from './apiKey.js';
 
+export const communicateWithOpenAI = (messages, pet) => {
 
-function communicateWithOpenAI (messages) {
-   //Aquí es donde debes implementar la petición con fetch o axios
+   //Aquí es donde se debe implementar la petición con fetch o axios
 
 
 return fetch('https://api.openai.com/v1/chat/completions', {
@@ -16,18 +16,17 @@ return fetch('https://api.openai.com/v1/chat/completions', {
         model: 'gpt-4o',
         messages: [
       {
-        'role': 'user',
-        'content': messages
-      }
+        'role': 'system', 'content': `Tu nombre es ${pet.name}. Tu personalidad es ${pet.description}. Responderás simulando ser la mascota asignada. La conversación debe ser fluida y natural. Siempre ten en cuenta tu nombre y personalidad para cada respuesta, incluye emojis. Tus respuestas serán cortas de no más de 30 palabras.`
+      },
+      {
+        "role": 'user',
+        "content": messages
+      },
     ],
         temperature: 0.9
-  
-    })
+    }),
 })
 
 .then(promiseOpenAI => {return promiseOpenAI.json()})
-.catch(error => {('Error de API Key:', error)});
-
-};
-
-export default communicateWithOpenAI;
+.catch(error => {('Error de API Key:', error)})
+}
