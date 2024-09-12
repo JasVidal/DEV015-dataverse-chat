c856a1e3ebcd67ecba51e0971cbb0a8820eb4d61
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-09-12 14:32:07 -0500

    actualizar: agregando estructura y estilos para vista de página Error, implementando la aplicación de estilos en index.html

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,8 +1,22 @@
+import { navigateTo } from "../router.js";
+
 const Error = () => {
   const error = document.createElement("section");
+  error.className = "error";
   error.innerHTML = `
-<div>Error 404: Page Not Found</div>
-</section>`;
+<div class="error-contain">
+  <h1 class="mensaje-error">Error 404: Page Not Found</h1>
+    <p class="texto-error">No podemos encontrar la página que estás buscando</p>
+    <button class="btn-volver">Volver al Inicio</button>
+</div>
+  </section>`;
+
+// Botón volver a Home //
+
+  const btnInicio = error.querySelector(".btn-volver");
+  btnInicio.addEventListener("click", () => {
+    navigateTo("/");
+  });
 
   return error;
 };
